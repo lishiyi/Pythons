827f0f3c01077f6cf60fac4d11f82556f5bb965f
lishiyi <lishiyiiii@gmail.com>
2016-06-07 18:51:15 -0400

    Mad Libs

---
+++ MadLibs/MadLibs.py
@@ -0,0 +1,27 @@
+'''Python Mad Libs'''
+print "Mad Libs Start!"
+
+name = raw_input("Please input your name:  ")
+print "We need 3 adjectives for the game."
+adj1 = raw_input("Please input the 1st adjective:  ")
+adj2 = raw_input("Please input the 2nd adjective:  ")
+adj3 = raw_input("Please input the 3rd adjective:  ")
+print "We need 3 verbs for the game."
+verb1 = raw_input("Please input the 1st verb:  ")
+verb2 = raw_input("Please input the 2nd verb:  ")
+verb3 = raw_input("Please input the 3rd verb:  ")
+print "We need 4 nouns for the game."
+noun1 = raw_input("Please input the 1st noun:  ")
+noun2 = raw_input("Please input the 2nd noun:  ")
+noun3 = raw_input("Please input the 3rd noun:  ")
+noun4 = raw_input("Please input the 4th noun:  ")
+animal = raw_input("Please input an animal:  ")
+food = raw_input("Please input a food:  ")
+fruit = raw_input("Please input a friut:  ")
+number = raw_input("Please input a number:  ")
+superhero = raw_input("Please input a superhero name:  ")
+country = raw_input("Please input a country:  ")
+dessert = raw_input("Please input a dessert:  ")
+year = raw_input("Please input an year:  ")
+
+print "This morning I woke up and felt %s because %s was going to finally %s over the big %s %s. On the other side of the %s were many %ss protesting to keep %s in stores. The crowd began to %s to the rythym of the %s, which made all of the %ss very %s. %s tried to %s into the sewers and found %s rats. Needing help, %s quickly called %s. %s appeared and saved %s by flying to %s and dropping %s into a puddle of %s. %s then fell asleep and woke up in the year %s, in a world where %ss ruled the world." %(adj1, name, verb1, adj2, noun1, noun2, animal, food, verb2, noun3, fruit, adj3, name, verb3, number, name, superhero, superhero, name, country, name, dessert, name, year, noun4)
